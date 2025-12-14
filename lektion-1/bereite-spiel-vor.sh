9e26f1b60da09824c163b403a38663d066394144
Jan Eisen <jan.eisen@tutanota.de>
2025-12-14 19:25:33 +0100

    Erste Lektion

---
+++ lektion-1/bereite-spiel-vor.sh
@@ -0,0 +1,9 @@
+mkdir -p spiel/{wald/{hoehle,lichtung,fluss},berg/{pfad,gipfel,hoehle},dorf/{haus,markt,turm},meer/{strand,insel/{palmen,hoehle}}} && \
+echo "🏆 SCHATZ GEFUNDEN! 🏆
+
+Wenn du das liest, hast du es geschafft!
+
+Sag Papa bitte:
+👉 'Der Schatz war auf der Insel im Meer, in der Höhle unter den Palmen.'
+
+Großartige Arbeit! 🎉" > spiel/meer/insel/hoehle/schatz.txt
